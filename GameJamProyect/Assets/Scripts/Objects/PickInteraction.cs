0ea35b1ba9936f9fd9c8e8e62e4ebdf24012c893
Carlos23 <carlos.enriquez.valerio@gmail.com>
2020-10-10 10:18:26 -0500

    Base of PlayerInteraction

---
+++ GameJamProyect/Assets/Scripts/Objects/PickInteraction.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickInteraction : InteractableObject
+{
+    public override void DoInteraction(GameObject player)
+    {
+        Debug.Log("Pick Interaction");
+
+    } 
+}
---
+++ GameJamProyect/Assets/Scripts/Player/PlayerInteraction.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    InteractableObject objectToInteract;
+    bool canInteract;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canInteract)
+        {
+            Debug.Log("Can Interact");
+            if (Input.GetKeyDown(KeyCode.E))
+                objectToInteract.DoInteraction(transform.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger");
+        objectToInteract = other.GetComponent<InteractableObject>();
+        canInteract = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        objectToInteract = null;
+        canInteract = false;
+    }
+}
