bcde32ae14e003dfe0b8bff4be754cb23d32d265
Carlos23 <carlos.enriquez.valerio@gmail.com>
2020-10-10 18:20:17 -0500

    Audio clips

--- GameJamProyect/Assets/Scripts/Player/PlayerMovement.cs
+++ GameJamProyect/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,25 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Rigidbody), typeof(AudioSource))]
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float speed;
+    [SerializeField] AudioClip hospitalMovementAudio;
+    [SerializeField] AudioClip carpetMovementAudio;
+    [SerializeField] AudioClip woodMovementAudio;
+
     Rigidbody rb;
+    AudioSource audioSource;
     Vector3 movement;
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = hospitalMovementAudio;
     }
     void FixedUpdate()
     {
         GetMovement();
         Move();
-       
+
     }
     void GetMovement()
     {
-        movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector2 axisValues = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Debug.Log(axisValues.magnitude);
+        if (axisValues.magnitude != 0&&!audioSource.isPlaying)
+            audioSource.Play();
+        movement = new Vector3(axisValues.x, 0, axisValues.y);
+
+
+
     }
 
     void Move()
@@ -29,4 +43,24 @@ public class PlayerMovement : MonoBehaviour
 
         rb.velocity = newVelocity;
     }
+
+    public void SetNewMovent(FloorMaterial floor)
+    {
+        switch (floor)
+        {
+            case FloorMaterial.WOOD:
+                audioSource.clip = woodMovementAudio;
+                break;
+            case FloorMaterial.CARPET:
+                audioSource.clip = carpetMovementAudio;
+                break;
+            case FloorMaterial.HOSPITAL:
+                audioSource.clip = hospitalMovementAudio;
+                break;
+            default:
+                break;
+        }
+    }
 }
+
+public enum FloorMaterial { WOOD, CARPET, HOSPITAL }
